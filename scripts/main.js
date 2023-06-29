10256e924355f0dace3e5897cdb426ca4dfe18e9
James Freeman <james@jammaloo.com>
2023-06-29 08:32:57 -0400

    feat: initial commit

---
+++ scripts/main.js
@@ -0,0 +1,45 @@
+// module aliases
+
+const Engine = Matter.Engine,
+    Render = Matter.Render,
+    Runner = Matter.Runner,
+    Bodies = Matter.Bodies,
+    Composite = Matter.Composite,
+    MouseConstraint = Matter.MouseConstraint;
+
+// create an engine
+const engine = Engine.create();
+
+// create a renderer
+const render = Render.create({
+    element: document.body,
+    engine: engine,
+    options: {
+        width: 1000,
+        height: 1000
+    }
+});
+const mouseConstraint = MouseConstraint.create(
+    engine, {element: document.querySelector("body")}
+);
+
+// create two boxes and a ground
+const boxA = Bodies.rectangle(400, 200, 80, 80);
+const boxB = Bodies.rectangle(450, 50, 80, 80);
+const gbottom = Bodies.rectangle(500, 1010, 1010, 60, { isStatic: true });
+const gleft = Bodies.rectangle(10, 500, 60, 1010, { isStatic: true });
+const gright = Bodies.rectangle(1010, 500, 60, 1010, { isStatic: true });
+const gtop = Bodies.rectangle(500, 10, 1010, 60, { isStatic: true });
+const stack = [boxA, boxB, gbottom, gleft, gright, gtop];
+Composite.add(engine.world, [stack, mouseConstraint]);
+// add all of the bodies to the world
+Composite.add(engine.world, stack);
+
+// run the renderer
+Render.run(render);
+
+// create runner
+const runner = Runner.create();
+
+// run the engine
+Runner.run(runner, engine);
